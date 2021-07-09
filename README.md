2244d487dc44e3214a03b599bf35078dfe910e09
ZeWaka <zewakagamer@gmail.com>
2021-07-08 22:48:01 -0700

    Initial POC commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# CommentAgent
+
+> A GitHub bot to trigger a configured GitHub action based on found text in a comment from an authorized user. Built with [probot](https://github.com/probot/probot) and [glitch](https://glitch.com).
+
+Current version: `0.1.0`
+
+Marketplace link: https://github.com/marketplace/commentagent
+
+## Installation
+
+After installation, create a `.github/commentagent.yml` file in the default branch to enable it.
+Example configuration:
+
+```yml
+# A mapping of keyword aliases to event type. Form of match:action name.
+# Required.
+aliasMappings:
+  "[BuildPlease]": "Build_Action"
+	?buildme: "Build_Action"
+  ?triage: triage
+
+# Determines if alias matching is case sensitive.
+# Optional. Defaults to true.
+caseSensitive: true
+
+# A mapping of action names to user group permissions.
+# Form of event type:group name or combined names (Explained lower in the README). 
+# Optional, defaults to MEMBER for each.
+permissionMappings:
+	"BuildAction": [MEMBER, CONTRIBUTOR, PRAUTHOR]
+	triage: 'CONTRIBUTOR'
+```
+
+## Usage
+You also need to set up an action triggered by the configuration-specified event type, example:
+```
+on:
+  repository_dispatch:
+    types: [Build_Action]
+
+jobs:
+	hello-world:
+    steps:
+      - run: echo "Hello World!"
+			- run: 'echo "PR Number: ${{ github.event.client_payload.pr_number }}"'
+			- run: 'echo "Comment Author: ${{ github.event.client_payload.comment_author }}"'
+			- run: 'echo "PR Head full repo name: ${{ github.event.client_payload.pr_head_full_repo_name }}"
+			- run: 'echo "PR Head ref: ${{ github.event.client_payload.pr_head_ref }}"'
+```
+As seen used above, the bot also sends some useful information to the action like the PR number and comment author login name as payload information.
+Feel free to request more sent information by filing an issue via GitHub.
+**Note:** This does not currently support pull request review comments for triggering, only normal PR comments.
+
+## Permission Field
+The permission system supports allowing multiple permission levels to trigger an action, such as both someone who has previously committed to the repository and members of the organization that owns the repository. For an explanation and list of the levels, see [here](https://docs.github.com/en/graphql/reference/enums#commentauthorassociation). The bot also supports `PRAUTHOR` for the author of the pull request.
+
+This is accomplished through the use of joining each level together with a pipe ("|"), as seen in the example configuration.
+
+## Contributing
+
+Read the [CONTRIBUTING](CONTRIBUTING.md) guide for information.
+
+## License
+
+Licensed under ISC. See [LICENSE](LICENSE) for more information.
---
+++ app.js
@@ -0,0 +1,105 @@
+module.exports = (app, { getRouter }) => {
+	app.on(
+		[
+			"issue_comment.created",
+			"issue_comment.edited"
+		],
+		async context => {
+			const config = await context.config("commentagent.yml", {
+				caseSensitive: true
+			});
+
+			if (!config) {
+				console.log('Config file named "commentagent.yml" not found. Exiting.');
+				return;
+			}
+
+			if (context.payload.issue != null) {
+				console.log('Not a PR. Exiting.')
+			}
+
+			if (typeof config.aliasMappings === 'undefined' || config.aliasMappings === null) {
+				console.log('No alias mappings specified in the config. Exiting.')
+			}
+
+			var issueComment = context.payload.issue_comment;
+
+			let commentBody = issueComment.comment.body.slice();
+			let PRNumber = issueComment.issue.number;
+			let repoName = issueComment.repository.full_name;
+
+			let commentAuthorName = issueComment.comment.user.login;
+			let commentAuthorAssociation = issueComment.comment.author_association;
+
+			let metadata = {
+				pr_number: PRNumber,
+				comment_author: commentAuthorName,
+				pr_head_full_repo_name: PRHeadFullName,
+				pr_head_ref: PRHeadRef,
+			}
+
+			let PRData = await octokit.request(`GET /repos/${repoName}/pulls/${PRNumber}`);
+			let PRHeadRef = PRData.head.ref;
+			let PRHeadFullName = PRData.head.full_name;
+			let PRAuthor = PRData.user.login;
+
+			if (config.caseSensitive === false) {
+				commentBody = commentBody.toLowerCase();
+			}
+
+			for (let token in config.aliasMappings) {
+				let tokenName = token.slice();
+				let eventName = config.labelMappings[token];
+
+				if (config.caseSensitive === false) {
+					tokenName = tokenName.toLowerCase();
+				}
+
+				if (commentBody.includes(tokenName)) {
+					if (isAuthorized(commentAuthorAssociation, eventName, PRAuthor, commentAuthorName)) {
+						sendDispatch(context, issueComment, eventName, metadata)
+					}
+				}
+			}
+		}
+	);
+};
+
+function sendDispatch(context, issueComment, event_type, metadata) {
+	context.octokit.rest.repos.createDispatchEvent(
+		{
+			owner: issueComment.repository.owner.login,
+			repo: issueComment.repository.name,
+			event_type: event_type,
+			client_payload: metadata
+		}
+	);
+}
+
+function isAuthorized(association, event, pr_author, comment_author) {
+	let map = config.permissionMappings;
+
+	// If no permission map is specified at all, default to member
+	if (typeof map === 'undefined' || map === null) {
+		if (association == 'MEMBER') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	if (association == 'PRAUTHOR' && pr_author == comment_author) {
+		return true
+	}
+
+	let permission = map[event];
+	if (Array.isArray(permission)) {
+		return permission.includes(association);
+	}
+	else if (typeof permission === 'string') {
+		return association == permission;
+	}
+	else {
+		return false;
+	}
+}
